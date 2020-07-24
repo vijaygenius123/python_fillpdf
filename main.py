5f5b6bd9041fdd03cde29c15be1f449e49737f3b
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-24 21:38:09 +0530

    Basic Integration

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+from pdfjinja import PdfJinja
+
+dirname = os.path.dirname(__file__)
+template_pdf_file = os.path.join(dirname, 'input.pdf')
+
+template_pdf = PdfJinja(template_pdf_file)
+
+rendered_pdf = template_pdf({
+    'Name': 'Vijay',
+    'Age': 26,
+    'Gender': 'Male'
+})
+
+output_file = os.path.join(dirname, 'output.pdf')
+
+rendered_pdf.write(open(output_file, 'wb'))
